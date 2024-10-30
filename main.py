fdb87e051be0c055c69b6854f4e36f77d193fa9a
Markus Zondler <contact@markuszondler.com>
2024-10-30 13:55:10 +0100

    feat: add game loop

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+import pygame
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
 def main():
     print("Starting asteroids!")
+    pygame.init()
+
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        # poll for events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # create screen
+        screen.fill("black")
+
+        # render game
+
+        # flip the display to put created scene on screen
+        pygame.display.flip()
+
+        clock.tick(60)  # limit to 60 FPS
+
+    pygame.quit()
 
 
 if __name__ == "__main__":
