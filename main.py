24d436958b2f5054713ece241b6ae83bb1f7f56e
Markus Zondler <contact@markuszondler.com>
2024-10-31 18:12:36 +0100

    feat: rotate player

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        player.update(dt)
 
         # create screen
         screen.fill("black")
